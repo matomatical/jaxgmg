cd70a833278e081259aaaf771be8ff96977e4b21
Matthew Farrugia-Roberts <m@far.in.net>
2024-06-18 18:14:34 +1000

    optimal return baselines wandb logging

--- jaxgmg/environments/cheese_in_the_corner.py
+++ jaxgmg/environments/cheese_in_the_corner.py
@@ -261,7 +261,7 @@ class Env(base.Env):
         * level : Level
                 The level to compute the optimal value for. Depends on the
                 wall configuration, the initial agent location, and the
-                location of each key and chest.
+                location of the cheese.
         * discount_rate : float
                 The discount rate to apply in the formula for computing
                 return.
