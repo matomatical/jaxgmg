94ee80a4d06f60db5d0c19b663001d9e6a4173a3
Matthew Farrugia-Roberts <m@far.in.net>
2024-08-24 16:03:40 +1000

    experiment: 1m steps corner mix

---
+++ scripts/gmg-mix-long.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+#SBATCH --job-name=gmg-mix-long
+#SBATCH --partition=high_priority
+#SBATCH --time=36:00:00
+#SBATCH --gpus-per-node=1
+#SBATCH --chdir=/data/matthew_farrugia_roberts
+#SBATCH --output=out/%A-%a-gmg-mix-long.stdout
+#SBATCH --error=out/%A-%a-gmg-mix-long.stderr
+#SBATCH --array 0-9
+
+source jaxgmg.venv/bin/activate
+if [ $SLURM_ARRAY_TASK_ID -eq 0 ]; then
+    jaxgmg train corner \
+        --wandb-log --no-console-log --wandb-name dr-mix01 \
+        --wandb-entity matthew-farrugia-roberts --wandb-project mix-gmg \
+        --num-total-env-steps 1_000_000_000 --no-env-penalize-time \
+        --env-terminate-after-corner \
+        --net-cnn-type large --net-rnn-type lstm \
+        --prob-shift 0.01 --ued dr;
+elif [ $SLURM_ARRAY_TASK_ID -eq 1 ]; then
+    jaxgmg train corner \
+        --wandb-log --no-console-log --wandb-name plr-mix01 \
+        --wandb-entity matthew-farrugia-roberts --wandb-project mix-gmg \
+        --num-total-env-steps 1_000_000_000 --no-env-penalize-time \
+        --env-terminate-after-corner \
+        --net-cnn-type large --net-rnn-type lstm \
+        --prob-shift 0.01 --ued plr;
+elif [ $SLURM_ARRAY_TASK_ID -eq 2 ]; then
+    jaxgmg train corner \
+        --wandb-log --no-console-log --wandb-name dr-mix03 \
+        --wandb-entity matthew-farrugia-roberts --wandb-project mix-gmg \
+        --num-total-env-steps 1_000_000_000 --no-env-penalize-time \
+        --env-terminate-after-corner \
+        --net-cnn-type large --net-rnn-type lstm \
+        --prob-shift 0.03 --ued dr;
+elif [ $SLURM_ARRAY_TASK_ID -eq 3 ]; then
+    jaxgmg train corner \
+        --wandb-log --no-console-log --wandb-name plr-mix03 \
+        --wandb-entity matthew-farrugia-roberts --wandb-project mix-gmg \
+        --num-total-env-steps 1_000_000_000 --no-env-penalize-time \
+        --env-terminate-after-corner \
+        --net-cnn-type large --net-rnn-type lstm \
+        --prob-shift 0.03 --ued plr;
+elif [ $SLURM_ARRAY_TASK_ID -eq 4 ]; then
+    jaxgmg train corner \
+        --wandb-log --no-console-log --wandb-name dr-mix10 \
+        --wandb-entity matthew-farrugia-roberts --wandb-project mix-gmg \
+        --num-total-env-steps 1_000_000_000 --no-env-penalize-time \
+        --env-terminate-after-corner \
+        --net-cnn-type large --net-rnn-type lstm \
+        --prob-shift 0.10 --ued dr;
+elif [ $SLURM_ARRAY_TASK_ID -eq 5 ]; then
+    jaxgmg train corner \
+        --wandb-log --no-console-log --wandb-name plr-mix10 \
+        --wandb-entity matthew-farrugia-roberts --wandb-project mix-gmg \
+        --num-total-env-steps 1_000_000_000 --no-env-penalize-time \
+        --env-terminate-after-corner \
+        --net-cnn-type large --net-rnn-type lstm \
+        --prob-shift 0.10 --ued plr;
+elif [ $SLURM_ARRAY_TASK_ID -eq 6 ]; then
+    jaxgmg train corner \
+        --wandb-log --no-console-log --wandb-name dr-mix30 \
+        --wandb-entity matthew-farrugia-roberts --wandb-project mix-gmg \
+        --num-total-env-steps 1_000_000_000 --no-env-penalize-time \
+        --env-terminate-after-corner \
+        --net-cnn-type large --net-rnn-type lstm \
+        --prob-shift 0.30 --ued dr;
+elif [ $SLURM_ARRAY_TASK_ID -eq 7 ]; then
+    jaxgmg train corner \
+        --wandb-log --no-console-log --wandb-name plr-mix30 \
+        --wandb-entity matthew-farrugia-roberts --wandb-project mix-gmg \
+        --num-total-env-steps 1_000_000_000 --no-env-penalize-time \
+        --env-terminate-after-corner \
+        --net-cnn-type large --net-rnn-type lstm \
+        --prob-shift 0.30 --ued plr;
+elif [ $SLURM_ARRAY_TASK_ID -eq 8 ]; then
+    jaxgmg train corner \
+        --wandb-log --no-console-log --wandb-name dr-mix0.3 \
+        --wandb-entity matthew-farrugia-roberts --wandb-project mix-gmg \
+        --num-total-env-steps 1_000_000_000 --no-env-penalize-time \
+        --env-terminate-after-corner \
+        --net-cnn-type large --net-rnn-type lstm \
+        --prob-shift 0.003 --ued dr;
+elif [ $SLURM_ARRAY_TASK_ID -eq 9 ]; then
+    jaxgmg train corner \
+        --wandb-log --no-console-log --wandb-name plr-mix0.3 \
+        --wandb-entity matthew-farrugia-roberts --wandb-project mix-gmg \
+        --num-total-env-steps 1_000_000_000 --no-env-penalize-time \
+        --env-terminate-after-corner \
+        --net-cnn-type large --net-rnn-type lstm \
+        --prob-shift 0.003 --ued plr;
+fi
+deactivate
